dda98fb21d40f0cd432d1d9a8cb45b8d4da242ca
Chad Moon <chad.d.moon@gmail.com>
2017-03-08 23:44:25 -0700

    deck settings

--- spinnaker/apply-config.sh
+++ spinnaker/apply-config.sh
@@ -1,4 +1,4 @@
 kubectl apply -f namespace.yml
 kubectl delete --namespace spinnaker configmap spinnaker-config
 sleep 2
-kubectl create --namespace spinnaker configmap spinnaker-config --from-file=config/front50.yml --from-file=config/clouddriver.yml --from-file=config/orca.yml  --from-file=config/gate.yml 
+kubectl create --namespace spinnaker configmap spinnaker-config --from-file=config/front50.yml --from-file=config/clouddriver.yml --from-file=config/orca.yml  --from-file=config/gate.yml  --from-file=config/settings.js 
