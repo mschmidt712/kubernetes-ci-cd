9cfd901c008e0206e70ca6040df229019c97caf7
Chad Moon <chad.d.moon@gmail.com>
2017-03-08 21:59:57 -0700

    clouddriver

--- spinnaker/rollout.sh
+++ spinnaker/rollout.sh
@@ -1,1 +1,4 @@
-kubectl --namespace spinnaker apply -f $1.yml; kubectl --namespace spinnaker rollout status deployment/$1
+./apply-config.sh
+kubectl --namespace spinnaker delete -f $1.yml
+kubectl --namespace spinnaker apply -f $1.yml
+kubectl --namespace spinnaker rollout status deployment/$1
