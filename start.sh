ddeedff00f097800ca30443efeb235b48a243438
Chad Moon <chad.d.moon@gmail.com>
2017-03-08 18:50:34 -0700

    jenkins spec update

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #minikube start --memory 6000 --cpus 2 --disk-size 40g  --insecure-registry "europa.192.168.99.100.xip.io:80"
 
-192.168.42.134
+#docker login -u TOKEN -p 0u5gzdtjn3m8102lmjxnt8t87u 192.168.42.134:30861
+
+# docker push 192.168.42.134:30861/nginx:latest
+
+# sudo vim /usr/lib/systemd/system/docker.service # --insecure-registry="192.168.42.134:30861"
+
+# deploy europa.192.168.42.134.xip.io:80/nginx:latest
