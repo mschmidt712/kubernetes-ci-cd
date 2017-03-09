71449ac110571cf4aa25a8767a10bfcf6bb03f01
Chad Moon <chad.d.moon@gmail.com>
2017-03-08 22:24:03 -0700

    clouddriver working

--- spinnaker/apply-config.sh
+++ spinnaker/apply-config.sh
@@ -1,3 +1,4 @@
 kubectl apply -f namespace.yml
 kubectl delete --namespace spinnaker configmap spinnaker-config
+sleep 2
 kubectl create --namespace spinnaker configmap spinnaker-config --from-file=config/front50.yml --from-file=config/clouddriver.yml
--- spinnaker/rollout.sh
+++ spinnaker/rollout.sh
@@ -1,4 +1,5 @@
 ./apply-config.sh
 kubectl --namespace spinnaker delete -f $1.yml
+
 kubectl --namespace spinnaker apply -f $1.yml
 kubectl --namespace spinnaker rollout status deployment/$1
