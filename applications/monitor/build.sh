0f6fd9fad3f7621d7c3dac6861ce14a8ef3ff16e
Evan Yeager <eyeager@kenzan.com>
2017-03-31 11:24:38 -0600

    Another try this time with kubectl added locally in the container

--- applications/monitor/build.sh
+++ applications/monitor/build.sh
@@ -3,6 +3,6 @@
 TAG=`git rev-parse --short HEAD`
 echo $TAG
 
-docker build --no-cache=true -t `minikube ip`:30912/monitor-scale:$TAG .
+docker build -t `minikube ip`:30912/monitor-scale:$TAG .
 docker push `minikube ip`:30912/monitor-scale:$TAG
 kubectl apply -f k8s/monitor-scale.yaml
