42526265315d935a66590841d8dd1654402321c4
Chad Moon <chad.d.moon@gmail.com>
2017-04-04 13:55:46 -0600

    cleanup

--- build.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-class Step:
-  def __init__(self, caption, command):
-    self.caption = name
-    self.command = command
-   
-  def displayCount(self):
-    print "Total Employee %d" % Employee.empCount
--- build.rb
+++
@@ -1,6 +0,0 @@
-require 'yaml'
-yml = YAML.load_file('docs.yml')
-
-@parts.each do |key, item|
-  puts key
-end
