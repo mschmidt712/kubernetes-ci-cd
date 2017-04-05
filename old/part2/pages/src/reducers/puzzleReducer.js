fbfe4951a3b65ded5d5e80f1b715d41af35e0fe4
Evan Yeager <eyeager@kenzan.com>
2017-04-05 14:59:13 -0600

    Updated puzzleReducer

--- old/part2/pages/src/reducers/puzzleReducer.js
+++ old/part2/pages/src/reducers/puzzleReducer.js
@@ -1,22 +1,52 @@
 import * as types from '../actions/actionTypes';
 
 const initialState = {
-  id: '',
+  fromCache: false,
+  fromMongo: false,
+  sendingData: false,
+  puzzleId: '',
   puzzleData: []
 };
 
 export default function puzzleReducer (state = initialState, action) {
   switch (action.type) {
-    case types.puzzle.GET_PUZZLE_DATA: {
+    case types.puzzle.GET_PUZZLE_DATA_SUCCESS: {
       return Object.assign({}, state, {
-        id: action.data.id,
         puzzleData: action.data.words
       });
     }
-    case types.puzzle.SUBMIT_PUZZLE_DATA: {
+    case types.puzzle.GET_PUZZLE_DATA_FAILURE: {
+      return state;
+    }
+    case types.puzzle.SUBMIT_PUZZLE_DATA_SUCCESS: {
       return Object.assign({}, state, {
-        puzzleData: action.data
+        puzzleData: action.data.words
       });
+    }
+    case types.puzzle.SUBMIT_PUZZLE_DATA_FAILURE: {
+      return state;
+    }
+    case types.puzzle.SENDING_DATA: {
+      return Object.assign({}, state, {
+        sendingData: action.data
+      });
+    }
+    case types.puzzle.FROM_MONGO: {
+      return Object.assign({}, state, {
+        fromMongo: action.data
+      });
+    }
+    case types.puzzle.FROM_CACHE: {
+      return Object.assign({}, state, {
+        fromCache: action.data
+      });
+    }
+    case types.websocket.PODS: {
+      console.log('pods emit handler', action.data.pods);
+      return Object.assign({}, state, { pods: [0, 1, 2, 4] });
+    }
+    case types.websocket.ACTIVE_INSTANCE: {
+      return Object.assign({}, state, { activePod: action.data.podId });
     }
     default: {
       return state;
