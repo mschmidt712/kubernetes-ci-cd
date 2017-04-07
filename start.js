aaa9a2db6ab2c7875535f3e9c42f229264b9c5e7
Miguel Rodriguez <mrodriguez@kenzan.com>
2017-04-07 13:23:03 -0600

    Cleanup

--- start.js
+++ start.js
@@ -4,56 +4,57 @@ YAML = require('yamljs');
 var inquirer = require('inquirer');
 var Rx = require('rx');
 const execSync = require('child_process').execSync;
-var environment = process.env;
 
 var config = {
     maxBuffer: 10000 * 1024,
-    env: environment
+    env: process.env
 };
 
 var prompts = new Rx.Subject();
-
 var stepIndex = 1;
 var commands = [];
 
 inquirer.prompt(prompts).ui.process.subscribe(
 
-  function (answers) {
-    answerIndex = answers.name*1
-    answerIndex = answerIndex - 1;
-    cmd = commands[answerIndex];
-    execSync(cmd, {stdio:[0,1,2], env: environment})
-},
-  function(err){
-    console.log('error')
-},
-  function(message){
-    console.log('complete')
-  }
+    function(answers) {
+        answerIndex = answers.name * 1
+        answerIndex = answerIndex - 1;
+        cmd = commands[answerIndex];
+        execSync(cmd, {
+            stdio: [0, 1, 2],
+            env: process.env
+        })
+    },
+    function(err) {
+        console.log('error')
+    },
+    function(message) {
+        console.log('complete')
+    }
 );
 
-prompts.onNext({type: 'confirm',name: "Begin", message: "Welcome to the Linux.com interactive Kubernetes tutorial by Kenzan. Press enter to begin\n", default: true});
+prompts.onNext({
+    type: 'confirm',
+    name: "Begin",
+    message: "Welcome to the Linux.com interactive Kubernetes tutorial by Kenzan. Press enter to begin\n",
+    default: true
+});
 
-
-YAML.load('steps.yml', function(docs)
-{
-    docList = docs.parts;
+YAML.load('steps.yml', function(docs) {
     var parts = docs.parts;
 
-    parts.forEach(function (item) {
-        var part = item.name;
-        var stepNum = 0;
+    parts.forEach(function(item) {
         var stepList = item.steps;
-        stepList.forEach(function (step) {
-            stepNum++;
+        stepList.forEach(function(step) {
             commands.push(step.com)
-            prompts.onNext({type: 'confirm',name: stepIndex, message: "\n \n \n" + part + " Step: " + stepNum + "\n" + step.cap + "\n \n" + step.com + "\n \nPress enter to the run the above command for the step.", default: true});
+            prompts.onNext({
+                type: 'confirm',
+                name: stepIndex,
+                message: "\n\n\n" + item.name + " Step: " + stepIndex + "\n" + step.cap + "\n\n" + step.com + "\n\nPress enter to the run the above command for the step.",
+                default: true
+            });
             stepIndex++;
         })
-
-    
     })
     prompts.onCompleted();
-
-
 });
