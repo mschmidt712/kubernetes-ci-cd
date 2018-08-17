09584d36b52607431e4a912454bdae477f54d8a1
David Zuluaga <dzuluaga@ebsco.com>
2018-08-17 13:44:00 -0600

    renamed etcd service manifest and references

--- scripts/etcd.sh
+++ scripts/etcd.sh
@@ -10,7 +10,7 @@ kubectl rollout status deploy/tiller-deploy -n kube-system
 kubectl  create -f manifests/etcd-cluster.yaml
 
 echo "installing etcd cluster service"
-kubectl  create -f manifests/service.json
+kubectl  create -f manifests/etcd-service.yaml
 
 echo "waiting for etcd cluster to turnup"
 sleep 10
